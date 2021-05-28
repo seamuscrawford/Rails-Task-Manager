d6fc1c179f713dfb7f7b60b91d7cab2410568889
seamus crawford <seamuscrawford@hotmail.co.uk>
2021-05-28 11:13:31 +0800

    ability to delete tasks added

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/tasks/:id", to: "tasks#show", as: :task
   get "/tasks/:id/edit", to: "tasks#edit", as: :task_edit
   patch "/tasks/:id", to: "tasks#update"
+  delete "/tasks/:id", to: "tasks#delete"
 end
