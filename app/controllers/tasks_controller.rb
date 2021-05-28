e8e9ae889b7f1de72958036ddd35c110378157ac
seamus crawford <seamuscrawford@hotmail.co.uk>
2021-05-28 11:03:49 +0800

    Able to edit and update tasks now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,9 +13,18 @@ class TasksController < ApplicationController
         redirect_to tasks_path
     end
     
-
     def show
         @task = Task.find(params[:id])
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to tasks_path(@task)
     end
 
     private
